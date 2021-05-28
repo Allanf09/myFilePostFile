63f30711c41e37a8df670c975e547b48ed1c7359
allan <allanfuenpi@criptext.com>
2021-05-27 20:12:57 -0500

    settings modified again.

--- shareapp/settings.py
+++ shareapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q$@0_)=x9rmqpa_+g+n^38(l#h#n_#oprc(ui@8-9(mht%u0-_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
