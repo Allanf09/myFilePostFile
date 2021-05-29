cba4a60952cff8606a79ec48b211b88088b6481c
allan <allanfuenpi@criptext.com>
2021-05-29 12:16:01 -0500

    Debug settings updated.

--- shareapp/settings.py
+++ shareapp/settings.py
@@ -143,7 +143,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import django_heroku
 django_heroku.settings(locals())
 
-if os.environ.get('DEBUG', True):
-    DEBUG = True
-elif os.environ.get('DEBUG', False):
-    DEBUG = False
+
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
